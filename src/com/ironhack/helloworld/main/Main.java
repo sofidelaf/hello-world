9d4a23041f8fddce2188952e167815178e60cc0c
Sofía de la Fuente <ec.sofiadelafuente@gmail.com>
2022-06-01 12:29:05 +0200

    Adds new feature

--- src/com/ironhack/helloworld/main/Main.java
+++ src/com/ironhack/helloworld/main/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Goodbye world!");
         System.out.println(productFormatter(" Esto es un nombre ", "  31/05/2022  "));
 
         /*
